845a02810cfecac92146bf2e465e8a89c6d76b6e
CyberKidzik <cyberkidz@yandex.com>
2024-08-21 13:21:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const flowerCards = document.querySelectorAll(".flower-card");
-
-    flowerCards.forEach(card => {
-        card.querySelector(".select-flower").addEventListener("click", function () {
-            const flowerName = card.getAttribute("data-flower-name");
-            const price = card.getAttribute("data-price");
-            const description = card.getAttribute("data-description");
-
-            const data = { flower_name: flowerName, price: price, description: description };
-
-            Telegram.WebApp.sendData(JSON.stringify(data));  // Отправка данных в Telegram WebApp
-        });
-    });
-});
+function orderBouquet(bouquetName, price) {
+    const data = {
+        bouquet_name: bouquetName,
+        price: price
+    };
+    Telegram.WebApp.sendData(JSON.stringify(data));
+}
