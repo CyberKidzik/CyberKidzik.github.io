14917ec378847a8fe8f15f3645679ec8691c1e9a
CyberKidzik <cyberkidz@yandex.com>
2024-08-21 11:45:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,98 +7,39 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+function updateMainButton(text, value) {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(text);
+        item = value;
+        tg.MainButton.show();
+    }
+}
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+const bouquets = [
+    { id: "btn1", text: "Вы выбрали Букет Роз!", item: "Букет Роз" },
+    { id: "btn2", text: "Вы выбрали Весенний Букет!", item: "Весенний Букет" },
+    { id: "btn3", text: "Вы выбрали Ромашковый Букет!", item: "Ромашковый Букет" },
+    { id: "btn4", text: "Вы выбрали Летний Букет!", item: "Летний Букет" },
+    { id: "btn5", text: "Вы выбрали Осенний Букет!", item: "Осенний Букет" },
+    { id: "btn6", text: "Вы выбрали Зимний Букет!", item: "Зимний Букет" }
+];
+
+bouquets.forEach(bouquet => {
+    let btn = document.getElementById(bouquet.id);
+    btn.addEventListener("click", function() {
+        updateMainButton(bouquet.text, bouquet.item);
+    });
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
 });
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
-
 
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
-
-
-
-
-
-
-
-
